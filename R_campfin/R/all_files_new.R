a4f6d88c59d9d28fd78b30d321129d9b83b68de1
Kiernan Nicholls <kiernann@protonmail.com>
2019-06-26 17:16:56 -0400

    Account for empty directories

--- R_campfin/R/all_files_new.R
+++ R_campfin/R/all_files_new.R
@@ -1,14 +1,16 @@
 all_files_new <- function(path = ".", pattern = NULL) {
-  all(
-    Sys.Date() ==
-      as.Date(
-        file.mtime(
-          list.files(
-            path = path,
-            pattern = pattern,
-            full.names = TRUE,
-          )
-        )
+  is_new <- Sys.Date() == as.Date(
+    file.mtime(
+      list.files(
+        path = path,
+        pattern = pattern,
+        full.names = TRUE,
       )
+    )
   )
+  if (!is_empty(is_new)) {
+    all(is_new)
+  } else {
+    !is_empty(is_new)
+  }
 }
