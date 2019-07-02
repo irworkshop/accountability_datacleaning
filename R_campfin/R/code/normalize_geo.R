bbe3bcc885803d1f53f44f6e2770e7a79847e4d0
Kiernan Nicholls <kiernann@protonmail.com>
2019-07-02 15:17:58 -0400

    Reorganize misc data, code, and docs

---
+++ R_campfin/R/code/normalize_geo.R
@@ -0,0 +1,89 @@
+# Normalize geographic values
+
+# addresses ----------------------------------------------------------------------------------
+
+normalize_address <- function(address, abbs = NULL, na = c("")) {
+
+  address_clean <- address %>%
+    str_to_upper() %>%
+    str_replace("-", " ") %>%
+    str_remove_all("[[:punct:]]") %>%
+    str_trim() %>%
+    str_squish() %>%
+    str_replace("P\\sO", "PO")
+
+  if (!is.null(abbs)) {
+    abbs <- as.data.frame(abbs)
+    for (i in seq_along(abbs[, 1])) {
+      address_clean <- str_replace(
+        string = address_clean,
+        pattern = str_c("\\b", abbs[i, 1], "\\b"),
+        replacement = abbs[i, 2]
+      )
+    }
+  }
+
+  address_clean[which(address_clean %in% na)] <- NA
+
+  return(address_clean)
+}
+
+# zip codes ----------------------------------------------------------------------------------
+
+normalize_zip <- function(zip, na = c("")) {
+
+  zip_clean <- zip %>%
+    str_remove_all("\\D") %>%
+    str_pad(width = 5, side = "left", pad = "0") %>%
+    str_sub(start = 1, end = 5)
+
+  return(zip_clean)
+}
+
+# state abbs ---------------------------------------------------------------------------------
+
+normalize_state <- function(state, valid = NULL, na = c("")) {
+
+  state_clean <- state %>%
+    str_to_upper() %>%
+    str_remove("[^A-z]") %>%
+    str_sub(start = 1, end = 2)
+
+  if (!is.null(valid)) {
+    state_clean[which(state_clean %in% na)] <- NA
+    state_clean[!(state_clean %in% valid)] <- NA
+  }
+
+  state_clean[which(state_clean %in% na)] <- NA
+
+  return(state_clean)
+}
+
+# cities -------------------------------------------------------------------------------------
+
+normalize_city <- function(city, abbs = NULL, na = c("")) {
+
+  city_clean <- city %>%
+    str_to_upper() %>%
+    str_replace("-", " ") %>%
+    str_remove_all("[[:punct:]]") %>%
+    str_remove("\\d+") %>%
+    str_trim() %>%
+    str_squish() %>%
+    na_if("")
+
+  if (!is.null(abbs)) {
+    abbs <- as.data.frame(abbs)
+    for (i in seq_along(abbs[, 1])) {
+      address_clean <- str_replace(
+        string = address_clean,
+        pattern = str_c("\\b", abbs[i, 1], "\\b"),
+        replacement = abbs[i, 2]
+      )
+    }
+  }
+
+  city_clean[which(city_clean %in% na)] <- NA
+
+  return(city_clean)
+}
