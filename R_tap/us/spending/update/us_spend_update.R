4670153bf16f23c605cc7e0a7cea64bbe38c7fe3
Kiernan Nicholls <kiernann@protonmail.com>
2021-03-02 17:37:52 -0500

    Update contract API script

--- R_tap/us/spending/update/us_spend_update.R
+++ R_tap/us/spending/update/us_spend_update.R
@@ -1,24 +1,22 @@
-# Mon Mar  1 17:12:09 2021 ------------------------------------------------
+# Tue Mar  2 14:49:34 2021 ------------------------------
 # Request US spending between two dates
 # Investigative Reporting Workshop
 # Public Accountability Project
 # Author: Kiernan Nicholls
-# For problems:
 #   kiernan@irworkshop.org
 #   kiernann@protonmail.com
 
-
+#!/usr/bin/env Rscript
+args <- commandArgs(trailingOnly = TRUE)
 here::i_am("us/spending/us_spend_update.R")
 
 # load packages -----------------------------------------------------------
+
 library(tidyverse)
 library(lubridate)
-library(jsonlite)
-library(gluedown)
 library(janitor)
 library(campfin)
 library(aws.s3)
-library(scales)
 library(glue)
 library(here)
 library(httr)
@@ -26,30 +24,40 @@ library(fs)
 
 # notes -------------------------------------------------------------------
 
-# Prior to FY19, Financial Assistance awards (grants, direct payments, loans,
-# insurance, and other financial assistance) only sporadically include Funding
-# Agency data.
-
-# Award Types: Contracts, Contract IDVs, Grants, Direct Payments, Loans,
-#   Insurance, Other Financial Assistance, Sub-Contracts, Sub-Grants
+# Award Types: All
 # Agency: Awarding Agency, All
 # Location: Recipient Location
 # Date Type: Action Date
-# File Format: TXT (Pipe Delimited)
+# File Format: TSV (Tab Delimited)
 
 # functions ---------------------------------------------------------------
 
-spend_api <- function(endpoint) {
-  modify_url(
+spend_api <- function(endpoint, ...) {
+  httr::modify_url(
     url = "https://api.usaspending.gov/",
-    path = c("api", "v2", endpoint)
+    path = c("api", "v2", endpoint),
+    ...
   )
 }
 
 # script args -------------------------------------------------------------
 
-if (length(args) == 0) {
+# call script from command line with (1) start and (2) end dates
+# Rscript --vanilla us_spend_update.R --args 2020-01-01 2020-12-31
 
+if (length(args) == 0) {
+  ### !!!!!!!!!!!!!!!!!!
+  # define dates here w/out args
+  # end_date <- "2020-10-01"
+  # start_date <- "2021-09-30"
+  end_date <- Sys.Date()
+  start_date <- end_date - 7
+  ### !!!!!!!!!!!!!!!!!!
+} else if (length(args) == 2){
+  end_date <- as.Date(args[2], tryFormats = c("%Y-%m-%d", "%m/%d/%Y"))
+  start_date <- as.Date(args[1], tryFormats = c("%Y-%m-%d", "%m/%d/%Y"))
+} else {
+  stop("when using args, use both start date then end date", call. = FALSE)
 }
 
 # prep data ---------------------------------------------------------------
@@ -59,34 +67,34 @@ award_types <- GET(spend_api("references/award_types"))
 # convert to data frame
 award_types <-
   content(award_types) %>%
-  map_df(enframe) %>%
-  unnest(cols = -name)
+  map_df(enframe, value = "award_type") %>%
+  unnest(cols = award_type) %>%
+  mutate(across(award_type, str_to_upper))
 
-kable(award_types)
+# make request ============================================================
 
-# make request ------------------------------------------------------------
+message(sprintf("requesting spending between %s and %s", start_date, end_date))
 
 award_post <- POST(
-  url = spend_api("bulk_download/awards"),
+  url = spend_api("bulk_download/awards/"),
   encode = "json",
   body = list(
-    # pipe delimited
-    file_format = "pstxt",
+    columns = list(),
+    file_format = "tsv",
     filters = list(
-      # all award types
       prime_award_types = award_types$name,
       sub_award_types = c("procurement", "grant"),
       date_type = "action_date",
       date_range = list(
-        # change for last update
-        start_date = "2020-10-01",
-        end_date = as.character(Sys.Date())
+        start_date = start_date,
+        end_date = end_date
       ),
-      # all award agencies
       agencies = list(
-        type = "awarding",
-        tier = "toptier",
-        name = "All"
+        list(
+          type = "awarding",
+          tier = "toptier",
+          name = "All"
+        )
       )
     )
   )
@@ -95,14 +103,15 @@ award_post <- POST(
 stop_for_status(award_post)
 post_date <- award_post$date
 award_post <- content(award_post)
-award_post$file_name
+raw_file <- award_post$file_name
+
+message(sprintf("status: requested (%s)", post_date))
 
 # check status ------------------------------------------------------------
 
 # All_PrimeTransactionsAndSubawards_2021-03-01_H21M36S10731192.zip
 
 while (!exists("post_status") || post_status == "running") {
-  message(sprintf("status: requested (%s)", post_date))
   # request status
   status_get <-GET(
     url = spend_api("bulk_download/status/"),
@@ -113,27 +122,43 @@ while (!exists("post_status") || post_status == "running") {
   status_get <- content(status_get)
   post_status <- status_get$status
   if (post_status == "running") {
-    message(sprintf("status: %s (%s)", post_status, Sys.time()))
+    message(glue("status: {post_status} ({Sys.time()}), waiting 5 min"))
     # wait 5 minutes and check again
-    Sys.sleep(5 * 60)
+    Sys.sleep(time = 5 * 60)
   } else {
-    message(sprintf("status: %s (%s)", post_status, Sys.time()))
+    message(sprintf("status: {post_status} ({Sys.time()})"))
   }
 }
+
 # download bulk zip when ready --------------------------------------------
 
-# check file size before download
-fs_bytes(paste(status_get$total_size, "KiB"))
+# check size before download
+# kilobytes to total bytes
+bulk_length <- status_get$total_size * 1000
 
-# download to a temp file
-raw_zip <- file_temp(ext = "zip")
-bulk_save <- GET(
-  url = award_post$file_url,
-  write_disk(path = raw_zip),
-  progress(type = "down")
-)
+# calculate download time
+if (isTRUE(requireNamespace("speedtest", quietly = TRUE))) {
+  speed <- speedtest::speedtest_cli(progress = FALSE)
+  dl <- speed$result[[7]]$download$bytes
+  dseconds(round(bulk_length / dl, 3))
+}
 
-# read file ---------------------------------------------------------------
+raw_dir <- dir_create(here("us", "spending", "update", "data"))
+raw_zip <- path(raw_dir, raw_file)
+message(glue("downloading bulk file: {raw_file} ({fs_bytes(bulk_length)})"))
+
+# download locally
+if (!file_exists(raw_zip)) {
+  bulk_save <- GET(
+    url = award_post$file_url,
+    write_disk(path = raw_zip),
+    progress(type = "down")
+  )
+}
+
+# extract files -----------------------------------------------------------
+
+message(glue("extracting zip archive to: {raw_dir}"))
 
 # list the zip contents
 unzip(raw_zip, list = TRUE) %>%
@@ -145,28 +170,89 @@ unzip(raw_zip, list = TRUE) %>%
   )
 
 # extract files
-raw_tsv <- unzip(raw_zip, exdir = dirname(raw_zip))
+all_tsv <- unzip(raw_zip, exdir = raw_dir)
 
-# read col types
-spend_cols <- read_csv(
-  file = "us/spending/update/usa_spend_cols.csv",
-  col_types = cols(
-    column = col_character(),
-    type = col_character()
+# contracts ===============================================================
+
+# list the contract files from zip
+con_tsv <- str_subset(all_tsv, "All_Contracts")
+con_n <- length(con_tsv)
+
+col_file <- here("us", "spending", "update", "con_cols.csv")
+if (file_exists(col_file)) {
+  # read col types
+  con_cols <- read_csv(col_file, col_types = cols())
+  con_cols <- str_c(con_cols$type, collapse = "")
+  ## as.col_spec(deframe(spend_cols))
+} else {
+  con_cols <- NULL
+}
+
+for (i in seq_along(con_tsv)) {
+  # read contract ---------------------------------------------------------
+  message(glue("reading contract file {i}/{con_n}"))
+
+  # read data frame
+  usc <- read_delim(
+    file = con_tsv[i],
+    delim = "\t",
+    guess_max = 0,
+    col_types = con_cols
   )
-)
 
-# as.col_spec(deframe(spend_cols))
-spend_cols <- str_c(spend_cols$type, collapse = "")
+  # tweak cols ------------------------------------------------------------
 
-# read data frame
-spending <- read_delim(
-  file = raw_txt[5],
-  delim = "|",
-  guess_max = 0,
-  col_types =
-)
+  # flag missing values
+  usc <- flag_na(
+    data = usc,
+    action_date, # date
+    federal_action_obligation, # amount
+    awarding_sub_agency_name, # agency
+    recipient_name # company
+  )
 
-# check rows and year
-unique(spending$action_date_fiscal_year)
-nrow(spending) == status_get$total_rows
+  # trim zip codes to 5 digits
+  usc <- mutate(usc, zip_clean = str_sub(recipient_zip_4_code, end = 5))
+
+  # add calendar year from action date
+  usc <- mutate(usc, action_year = year(action_date))
+
+  # save checks -----------------------------------------------------------
+  message(glue("checking contract file {i}/{con_n}"))
+
+  check <- tibble(
+    file = basename(con_tsv[i]),
+    start_date = min(usc$action_date, na.rm = TRUE),
+    end_date = max(usc$action_date, na.rm = TRUE),
+    nrow = nrow(usc),
+    ncol = ncol(usc),
+    n_types = n_distinct(usc$award_type_code),
+    fiscal_year = unique(usc$action_date_fiscal_year)[1],
+    sum_amt = sum(usc$federal_action_obligation, na.rm = TRUE),
+    na_flags = sum(usc$na_flag, na.rm = TRUE),
+    zero_amt = sum(usc$federal_action_obligation <= 0, na.rm = TRUE),
+    city_good = round(prop_in(usc$recipient_city_name, valid_city), 4),
+    state_good = round(prop_in(usc$recipient_state_code, valid_state), 4),
+    zip_good = round(prop_in(usc$zip_clean, valid_zip), 4)
+  )
+
+  # save contract ---------------------------------------------------------
+  con_out <- glue("us_contract_{start_date}-{end_date}-{i}.csv")
+  message(glue("writing contract file {i}/{con_n} to: {con_out}"))
+
+  write_delim(
+    x = usc,
+    file = ,
+    delim = ",",
+    na = "",
+    quote_escape = "double",
+  )
+  rm(usc)
+  Sys.sleep(time = 10)
+  flush_memory(n = 2)
+
+}
+
+# assistance ==============================================================
+
+# upload ==================================================================
