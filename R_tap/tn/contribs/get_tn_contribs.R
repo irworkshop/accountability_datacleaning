0467953fad0c640561cde6c98ca88a3480a002b2
Kiernan Nicholls <kiernann@protonmail.com>
2021-08-13 13:34:04 -0400

    Split TN contrib addresses

--- R_tap/tn/contribs/get_tn_contribs.R
+++ R_tap/tn/contribs/get_tn_contribs.R
@@ -1,10 +1,12 @@
 library(tidyverse)
+library(janitor)
+library(campfin)
 library(rvest)
 library(httr)
 library(cli)
 library(fs)
 
-tn_dir <- "~/Desktop/tn"
+tn_dir <- "~/Desktop/tn_contribs/"
 
 for (y in 2000:2021) {
   cli_h2("Year: {y}")
@@ -120,3 +122,132 @@ for (y in 2000:2021) {
 
   Sys.sleep(runif(1, 10, 30))
 }
+
+tn_csv <- dir_ls(tn_dir, glob = "*.csv")
+
+# read together -----------------------------------------------------------
+
+tnc <- map_df(
+  .x = tn_csv,
+  .f = function(x) {
+    with_edition(
+      edition = 1,
+      code = read_delim(
+        file = x,
+        delim = ",",
+        escape_backslash = TRUE,
+        escape_double = FALSE,
+        col_types = cols(
+          .default = col_character(),
+          `Amount` = col_number(),
+          `Date` = col_date("%m/%d/%Y"),
+          `Election Year` = col_integer()
+        )
+      )
+    )
+  }
+)
+
+tnc <- clean_names(tnc, case = "snake")
+n_distinct(tnc$type) == 2
+
+# split address -----------------------------------------------------------
+
+x3 <- tnc %>%
+  distinct(contributor_address) %>%
+  separate(
+    col = contributor_address,
+    into = c("addr_city", "state_zip"),
+    sep = "\\s,\\s(?=[^,]*,[^,]*$)",
+    remove = FALSE,
+    extra = "merge",
+    fill = "left"
+  ) %>%
+  separate(
+    col = state_zip,
+    into = c("state", "zip"),
+    sep = ",\\s(?=\\d)",
+    extra = "merge",
+    fill = "left"
+  )
+
+good_split <- filter(x3, state %in% valid_abb)
+bad_split <- filter(x3, state %out% valid_abb)
+
+# fix split ---------------------------------------------------------------
+
+
+# mising something in the middle, move and re-split
+no_zip <- bad_split %>%
+  filter(is.na(state) & is.na(addr_city) & str_detect(zip, "\\s\\w{2}$")) %>%
+  select(-addr_city, -state) %>%
+  separate(
+    col = zip,
+    into = c("addr_city", "state"),
+    sep = "\\s?,\\s?(?=[^,]*$)",
+    extra = "merge",
+    fill = "right"
+  )
+
+# remove fixed from bad
+bad_split <- bad_split %>%
+  filter(contributor_address %out% no_zip$contributor_address)
+
+# no zip, city-state moved to end, split-merge city into addr
+no_zip <- bad_split %>%
+  filter(!is.na(addr_city) & is.na(state) & str_detect(zip, "\\s\\w{2}$")) %>%
+  separate(
+    col = zip,
+    into = c("city", "state"),
+    sep = "\\s+,\\s"
+  ) %>%
+  unite(
+    col = addr_city,
+    ends_with("city"),
+    sep = ", "
+  ) %>%
+  bind_rows(no_zip)
+
+bad_split <- bad_split %>%
+  filter(contributor_address %out% no_zip$contributor_address)
+
+# no state, addr moved to state, move to addr and remove state
+no_state <- bad_split %>%
+  filter(is.na(addr_city) & !is.na(state) & str_detect(zip, "^\\d{5,}")) %>%
+  select(-addr_city) %>%
+  rename(addr_city = state)
+
+bad_split <- bad_split %>%
+  filter(contributor_address %out% no_state$contributor_address)
+
+# combine everything and extract states
+full_bad <- bad_split %>%
+  filter(is.na(state) | nchar(state) != 2) %>%
+  unite(
+    -contributor_address,
+    col = addr_city,
+    sep = ", ",
+    na.rm = TRUE
+  ) %>%
+  mutate(
+    state = str_extract(addr_city, "^[A-Z]{2}$"),
+    addr_city = na_if(str_remove(addr_city, "^[A-Z]{2}$"), "")
+  )
+
+bad_split <- bad_split %>%
+  filter(contributor_address %out% full_bad$contributor_address)
+
+# remaining just have bad states in general
+bad_split %>%
+  count(state, sort = TRUE)
+
+# recombine fixes and fill with empty cols
+bad_fix <- bind_rows(no_zip, no_state, full_bad, bad_split)
+bad_fix <- mutate(bad_fix, across(.fns = str_squish))
+
+sample_n(bad_fix, 20)
+
+# recombine with good splits
+tn_addr <- bind_rows(good_split, bad_fix)
+
+write_tsv(tn_addr, file = "~/Desktop/tn_addr.tsv")
