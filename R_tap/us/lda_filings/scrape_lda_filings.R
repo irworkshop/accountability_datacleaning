eb811ad8b193eca7cd77f71edf9f061871ebbfe9
Kiernan Nicholls <kiernann@protonmail.com>
2021-10-28 11:59:11 -0400

    Continue LDA filing scrape from last file

--- R_tap/us/lda_filings/scrape_lda_filings.R
+++ R_tap/us/lda_filings/scrape_lda_filings.R
@@ -3,10 +3,11 @@
 # https://lda.senate.gov/api/
 # Tue Jul 27 14:21:30 2021 ------------------------------------------------
 
-library(readr)
-library(httr)
-library(cli)
-library(fs)
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(jsonlite))
+suppressPackageStartupMessages(library(httr))
+suppressPackageStartupMessages(library(cli))
+suppressPackageStartupMessages(library(fs))
 
 cli_h1("Save senate lobbyist filings")
 cli_ol()
@@ -44,34 +45,42 @@ cli_quit <- function(text) {
 
 # arguments ---------------------------------------------------------------
 # Argument order:
-#   1. CSV file path to continuously save rows
+#   1. Directory to which JSON files are saved
 #   2. Date after which filings are posted (optional)
 #   3. The alphanumeric API key (optional)
 
-# Rscript sen_lob_fil.R --args {output file} {from date} {api key}
-# Rscript sen_lob_fil.R --args sen_lob.csv 2021-01-01 abcdefg123456
+# Rscript sen_lob_fil.R --args {output dir} {from date} {api key}
+# Rscript sen_lob_fil.R --args ~/Documents 2021-01-01 abcdefg123456
 
 cli_h2("Get command arguments")
 # capture output file from command line
 cmd_args <- commandArgs(trailingOnly = TRUE)
 if (length(cmd_args) >= 1 && grepl("csv$", cmd_args[1])) {
-  lob_csv <- cmd_args[1]
+  lob_dir <- cmd_args[1]
 } else {
-  lob_csv <- file.path(getwd(), "senate_lobbyist_filings.csv")
+  lob_dir <- file.path(getwd(), "data")
 }
 
-cli_alert_info("Saving filings to {.path {lob_csv}}")
+cli_alert_info("Saving filings to {.path {lob_dir}}")
+
+has_dir <- dir.exists(lob_dir)
+dir_txt <- ifelse(has_dir, "exists", "created")
+dir.create(path = lob_dir, showWarnings = FALSE)
+
+cli_alert_success(sprintf("{.path %s} directory %s", lob_dir, dir_txt))
 
 if (length(cmd_args) >= 2) {
   dt_from <- ifelse(grepl("^\\d{4}", cmd_args[2]), cmd_args[2], NULL)
   cli_alert_info("Requesting filings from {dt_from}")
 } else {
   dt_from <- NULL
+  cli_alert_info("Requesting all filings")
 }
+
 
 if (length(cmd_args) >= 3) {
   api_key <- ifelse(nchar("csv$") > 20, cmd_args[3], "")
-  cli_alert_info("Using API key: {col_grey(api_key)}")
+  cli_alert_info("Using API key: {col_red(api_key)}")
 } else {
   api_key <- Sys.getenv("LDA_API_KEY")
 }
@@ -101,32 +110,59 @@ if (length(cmd_args) >= 3) {
 cli_h2("Request data")
 # filing contains data on registrant and client
 
-n_pg <- 1
-n_row <- 0
+pg_size <- 250
 
-# request first page
-cli_h3("Page number: {n_pg}")
-fil_get <- GET(
-  url = "https://lda.senate.gov/api/v1/filings/",
-  query = list(page_size = 250, filing_dt_posted_after = dt_from),
-  auth_lda()
-)
+# check for existing data
+exist_json <- dir_ls(lob_dir, glob = "*.json")
+if (length(exist_json) > 0) {
+  exist_row <- str_extract_all(exist_json, "\\d+", simplify = TRUE)
+  exist_row <- matrix(
+    as.numeric(exist_row),
+    ncol = ncol(exist_row)
+  )
 
-# convert request to dataframe
-fil_dat <- content(fil_get, "parsed", simplifyDataFrame = TRUE, flatten = TRUE)
+  max_json <- exist_json[which.max(exist_row[, 2])]
+  fil_dat <- fromJSON(max_json)
+  n_row <- max(exist_row[, 2])
+  n_pg <- as.integer(str_extract(fil_dat$previous, "\\d+(?=&)")) + 1
 
-# how many rows and pages of 25
-all_row <- fil_dat$count
-all_pg <- ceiling(fil_dat$count/250)
+  all_row <- fil_dat$count
+  all_pg <- ceiling(fil_dat$count/pg_size)
+  prop_row <- paste0(round(100 * (n_row / all_row), 2), "%")
 
-cli_alert_info("Total record count: {all_row} ({all_pg} pages)")
+} else {
+  n_pg <- 1
+  n_row <- 0
 
-# update progress check
-n_row <- n_row + nrow(fil_dat$results)
-prop_row <- paste0(round(100 * (n_row/all_row), 2), "%")
+  # request first page
+  cli_h3("Page number: {n_pg}")
+  fil_get <- GET(
+    url = "https://lda.senate.gov/api/v1/filings/",
+    query = list(page_size = pg_size, filing_dt_posted_after = dt_from),
+    auth_lda()
+  )
 
-# write results to file
-write_csv(fil_dat$results, file = lob_csv)
+  # convert request to dataframe
+  fil_txt <- content(fil_get, as = "text", encoding = "UTF-8")
+  fil_dat <- content(fil_get, as = "parsed")
+
+  # how many rows and pages of 25
+  all_row <- fil_dat$count
+  all_pg <- ceiling(fil_dat$count/pg_size)
+
+  cli_alert_info("Total record count: {all_row} ({all_pg} pages)")
+
+  # create file name
+  new_row <- length(fil_dat$results)
+  lob_json <- sprintf("lda-filing_%i-%i.json", n_row + 1, n_row + new_row)
+
+  # update progress check
+  n_row <- n_row + new_row
+  prop_row <- paste0(round(100 * (n_row / all_row), 2), "%")
+
+  # write results to file
+  write_file(x = prettify(fil_txt), file = file.path(lob_dir, lob_json))
+}
 
 # next page ---------------------------------------------------------------
 
@@ -138,11 +174,14 @@ while (has_next) {
   # request next page if exists
   fil_get <- GET(fil_dat[["next"]], auth_lda())
   # repeat binding
-  fil_dat <- content(fil_get,"parsed", simplifyDataFrame = TRUE, flatten = TRUE)
+  fil_txt <- content(fil_get, "text", encoding = "UTF-8")
+  fil_dat <- content(fil_get, "parsed")
   has_next <- !is.null(fil_dat[["next"]])
-  # append to previous page results
-  write_csv(fil_dat$results, file = lob_csv, append = TRUE, progress = FALSE)
-  n_row <- n_row + nrow(fil_dat$results)
-  prop_row <- paste0(round(100 * (n_row/all_row), 2), "%")
+  new_row <- length(fil_dat$results)
+  lob_json <- sprintf("lda-filing_%i-%i.json", n_row + 1, n_row + new_row)
+  # write to new file
+  write_file(prettify(fil_txt), file.path(lob_dir, lob_json))
+  n_row <- n_row + new_row
+  prop_row <- paste0(round(100 * (n_row / all_row), 2), "%")
   cli_alert_success("Total results written: {n_row} ({prop_row})")
 }
